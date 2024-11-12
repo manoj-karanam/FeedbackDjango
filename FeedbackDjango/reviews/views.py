264734e1dac5556877068e31e50bf25defbe34dc
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2024-11-12 17:06:53 -0500

    added TemplateViews

--- FeedbackDjango/reviews/views.py
+++ FeedbackDjango/reviews/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .forms import ReviewForm
 from django.views import View
+from django.views.generic.base import TemplateView
+
+from .forms import ReviewForm
+from .models import Review
 
 
 # Create your views here.
@@ -19,10 +22,6 @@ class ReviewView(View):
         return render(request, "reviews/reviews.html",{
         "form":form})
     
-        
-
-
-
 
 # def review(request):
 #     if request.method == 'POST':
@@ -40,5 +39,30 @@ class ReviewView(View):
 #     })
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
+
+class ThankYouView(TemplateView):
+    template_name="reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context["message"] = "This Works!"
+        return context
+    
+class ReviewListView(TemplateView):
+    template_name = "reviews/review_list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews']= reviews
+        return context
+    
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        context['review']= Review.objects.get(pk=review_id)
+        return context
+ 
