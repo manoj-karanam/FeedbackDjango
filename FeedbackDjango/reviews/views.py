10fb5a49c23defda585761b5dde8cc6f8d9f71e4
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2024-11-15 10:26:53 -0500

    added session with favorite constraint for the posts

--- FeedbackDjango/reviews/views.py
+++ FeedbackDjango/reviews/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView, DetailView, FormView, CreateView
+from django.views.generic import ListView, DetailView, FormView, CreateView, View
 
 from .forms import ReviewForm
 from .models import Review
@@ -36,4 +36,18 @@ class ReviewListView(ListView):
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+    
  
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        request.session["favorite_review"]= review_id
+        return HttpResponseRedirect("/reviews/"+review_id)
